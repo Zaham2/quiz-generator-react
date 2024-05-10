d3030613419ffe23766ec845711198c140221260
Zaham2 <zaham2@yahoo.com>
2024-05-10 09:49:36 +0300

    WIP: Added Client-Side routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import QuizContainer from './components/QuizContainer';
-import { useEffect, useState } from 'react';
-import fetchData from './services/data';
+import Home from './pages/Home';
 
 function App() {
 
-  const [data, setData] = useState([])
-
-  useEffect(() => {
-    setData(fetchData())
-  }, [])
   return (
     <div>
-      <QuizContainer>
-
-      </QuizContainer>
+      <Home />
     </div>
   );
 }
